8d28f1c21302315af5e99355e0f3bc720c94634a
SabinaTalibova <sabinatalibova1@gmail.com>
2017-07-08 23:38:57 +0400

    Add files via upload

---
+++ src/SumOfDigits.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class SumOfDigits {
+
+	public static void main(String[] args) {
+		//Problem Statement 
+		//Input data are in the following format:
+		//
+		// first line contains N - the number of values to process;
+		// and then N lines will follow describing the values for which sum of
+		// digits should be calculated by 3 integers A B C;
+		// for each case you need to multiply A by B and add C (i.e. A * B + C)
+		// - then calculate sum of digits of the result.
+		// Answer should have N results, also separated by spaces.
+		
+		Scanner sc=new Scanner(System.in);
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
