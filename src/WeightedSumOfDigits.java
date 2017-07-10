c1bcbc8ca270b17c574a9b79f7537e54d5c6dc9c
SabinaTalibova <sabinatalibova1@gmail.com>
2017-07-10 23:37:59 +0400

    Add files via upload

---
+++ src/WeightedSumOfDigits.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class WeightedSumOfDigits {
+
+	public static void main(String[] args) {
+		
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		for(int i=0;i<n;i++){
+			int a=sc.nextInt();
+			while(a>0){
+				int m=a%10;
+				
+			}
+			
+		}
+
+	}
+}
